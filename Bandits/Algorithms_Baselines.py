40bde046b8ee007f30f098e707ac1cd40577fb89
FromageChaud <lucas.gerretsen2@gmail.com>
2018-01-31 10:37:52 +0100

    2.1-3
    UCB-3

--- Bandits/Algorithms_Baselines.py
+++ Bandits/Algorithms_Baselines.py
@@ -22,19 +22,29 @@ class FTL:
         return "FTL"
 
 
-class UCB:
+class UCB: # TP 2.1
     def __init__(self, nbArms, maxReward=1.):
-        ...
+        self.A = nbArms
+        self.Alpha = 3
+        self.MaxReward = maxReward # les rewards seront normalisées sur [0,1]
         self.clear()
 
     def clear(self):
-        ...
+        self.NbPulls = np.zeros(self.A)
+        self.Means = np.ones(self.A)
+        self.Time = 1
 
     def chooseArmToPlay(self):
-        return ...
+        if self.Time<=self.A: # Draw each arm once
+            return self.Time-1
+        else:
+            return np.argmax(self.Means+np.sqrt(self.Alpha*np.log(self.Time)/(2*self.NbPulls)))
 
     def receiveReward(self, arm, reward):
-        ...
+        reward = reward/self.MaxReward
+        self.Means[arm] = (self.Means[arm]*self.NbPulls[arm]+reward)/(self.NbPulls[arm]+1.)
+        self.NbPulls[arm] = self.NbPulls[arm] +1
+        self.Time += 1
 
     def name(self):
         return "UCB"
